2abd498eca75920949ae578b9d3a23ad888f5ad4
SaurabhjadhavKH <saurabhjadhav9308@gmail.com>
2021-08-28 18:31:28 +0530

    Index Page Testing

--- src/main/java/com/meettherider/controller/UserController.java
+++ src/main/java/com/meettherider/controller/UserController.java
@@ -1,5 +1,15 @@
 package com.meettherider.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+	
+	@GetMapping("/")
+	public String indexpage() {
+		
+		return "index";
+	}
 
 }
